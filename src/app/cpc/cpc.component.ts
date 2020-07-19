de49c27e707df4c318777a4e3c71d34fa65b3ae1
Doctor119 <mehahne@gmail.com>
2020-07-19 16:33:53 -0600

    created view for cpc search

--- src/app/cpc/cpc.component.ts
+++ src/app/cpc/cpc.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cpc.component.scss']
 })
 export class CpcComponent implements OnInit {
+  cpcSearchOption: string = 'none';
+  displayTreeSearch: boolean = false;
+  displayKeywordSearch: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onItemChange(newValue: string): void {
+    if (newValue === 'searchTree') {
+      this.displayKeywordSearch = false;
+      this.displayTreeSearch = true;
+    }
+    else if (newValue === 'searchKeyword') {
+      this.displayKeywordSearch = true;
+      this.displayTreeSearch = false;
+    }
+  }
+
+  goDownALevel(newLevel: string) {
+
+  }
+
+  goUpALevel(newLevel: string) {
+    
+  }
 }
